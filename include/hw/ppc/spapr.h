4f1bb8496cbd7b1dd40a97762f4d1d2ddfcc3968
Miroslav Rezanina <mrezanin@redhat.com>
2018-10-19 13:27:13 +0200

    Add ppc64 machine types
    Adding changes to add RHEL machine types for ppc64 architecture.
    
    Signed-off-by: Miroslav Rezanina <mrezanin@redhat.com>
    
    Rebase changes (4.0.0):
    - remove instance options and use upstream solution
    - Use upstream compat handling
    - Replace SPAPR_PCI_2_7_MMIO_WIN_SIZE with value (changed upstream)
    - re-add handling of instance_options (removed upstream)
    - Use p8 as default for rhel machine types (p9 default upstream)
    - sPAPRMachineClass renamed to SpaprMachineClass (upstream)
    
    Rebase changes (4.1.0):
    - Update format for compat structures
    
    Rebase notes (weekly-210303):
    - Use rhel-8.4.0 hw compat
    
    Merged patches (4.0.0):
    - 467d59a redhat: define pseries-rhel8.0.0 machine type
    
    Merged patches (4.1.0):
    - f21757edc target/ppc/spapr: Enable mitigations by default for pseries-4.0 machine type
    - 2511c63 redhat: sync pseries-rhel7.6.0 with rhel-av-8.0.1
    - 89f01da redhat: define pseries-rhel8.1.0 machine type
    
    Merged patches (4.2.0):
    - bcba728 redhat: update pseries-rhel8.1.0 machine type
    - redhat: update pseries-rhel-7.6.0 machine type (patch 93039)
    - redhat: define pseries-rhel8.2.0 machine type (patch 93041)
    
    Merged patches (5.1.0):
    - eb121ff spapr: Enable DD2.3 accelerated count cache flush in pseries-5.0 machine (partial)
    
    Merged patches (5.2.0 rc0):
    - 311a20f redhat: define pseries-rhel8.3.0 machine type
    - 1284167 ppc: Set correct max_cpus value on spapr-rhel* machine types
    - 1ab8783 redhat: update pseries-rhel8.2.0 machine type
    - b162af531a target/ppc: Add experimental option for enabling secure guests
    
    Merged patches (weekly-201216):
    - 943c936df3 redhat: Add spapr_machine_rhel_default_class_options()
    - 030b5e6fba redhat: Define pseries-rhel8.4.0 machine type
    
    Merged patches (weekly-210602):
    - b7128d8ef7 redhat: Define pseries-rhel8.5.0 machine type

--- include/hw/ppc/spapr.h
+++ include/hw/ppc/spapr.h
@@ -146,6 +146,7 @@ struct SpaprMachineClass {
     bool pre_5_1_assoc_refpoints;
     bool pre_5_2_numa_associativity;
 
+    bool has_power9_support;
     bool (*phb_placement)(SpaprMachineState *spapr, uint32_t index,
                           uint64_t *buid, hwaddr *pio,
                           hwaddr *mmio32, hwaddr *mmio64,
@@ -229,6 +230,9 @@ struct SpaprMachineState {
 
     /* Set by -boot */
     char *boot_device;
+ 
+    /* Secure Guest support via x-svm-allowed */
+    bool svm_allowed;
 
     /*< public >*/
     char *kvm_type;
