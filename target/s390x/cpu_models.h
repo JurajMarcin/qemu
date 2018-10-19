6a14fc5e35a8cec7f049c203d6dc2390fac175f1
Miroslav Rezanina <mrezanin@redhat.com>
2018-10-19 13:47:32 +0200

    Add s390x machine types
    Adding changes to add RHEL machine types for s390x architecture.
    
    Signed-off-by: Miroslav Rezanina <mrezanin@redhat.com>
    --
    
    Rebase changes (7.1.0 rc0):
    - Moved adding rhel_old_machine_deprecation variable to general machine types commit
    
    Merged patches (6.1.0):
    - 64a9a5c971 hw/s390x: Remove the RHEL7-only machine type
    - 395516d62b redhat: s390x: add rhel-8.5.0 compat machine
    
    Merged patches (6.2.0):
    - 3bf66f4520 redhat: Add s390x machine type compatibility update for 6.1 rebase
    
    Merged patches (7.0.0):
    - e6ff4de4f7 redhat: Add s390x machine type compatibility handling for the rebase to v6.2
    - 4b0efa7e21 redhat: Add rhel8.6.0 and rhel9.0.0 machine types for s390x
    - dcc64971bf RHEL: mark old machine types as deprecated (partialy)
    
    Merged patches (7.1.0 rc0):
    - 1d6439527a WRB: Introduce RHEL 9.0.0 hw compat structure (only hw/s390x/s390-virtio-ccw.c chunk)
    - c8ad21ca31 redhat: Update s390x machine type compatibility for rebase to QEMU 7.0.0
    - 5bcf8d874c target/s390x: deprecate CPUs older than z14

--- target/s390x/cpu_models.h
+++ target/s390x/cpu_models.h
@@ -38,6 +38,8 @@ struct S390CPUDef {
     S390FeatBitmap full_feat;
     /* used to init full_feat from generated data */
     S390FeatInit full_init;
+    /* if deprecated, provides a suggestion */
+    const char *deprecation_note;
 };
 
 /* CPU model based on a CPU definition */
