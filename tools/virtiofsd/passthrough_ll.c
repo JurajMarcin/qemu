8ce8ccc2a22798a89bac06a37427c3a3cea91a62
Dr. David Alan Gilbert <dgilbert@redhat.com>
2020-05-05 16:35:54 +0100

    tools/virtiofsd/passthrough_ll: Fix double close()
    RH-Author: Dr. David Alan Gilbert <dgilbert@redhat.com>
    Message-id: <20200505163600.22956-2-dgilbert@redhat.com>
    Patchwork-id: 96269
    O-Subject: [RHEL-AV-8.2.1 qemu-kvm PATCH 1/7] tools/virtiofsd/passthrough_ll: Fix double close()
    Bugzilla: 1817445
    RH-Acked-by: Stefan Hajnoczi <stefanha@redhat.com>
    RH-Acked-by: Max Reitz <mreitz@redhat.com>
    RH-Acked-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    RH-Acked-by: Michael S. Tsirkin <mst@redhat.com>
    
    From: Philippe Mathieu-Daudé <philmd@redhat.com>
    
    On success, the fdopendir() call closes fd. Later on the error
    path we try to close an already-closed fd. This can lead to
    use-after-free. Fix by only closing the fd if the fdopendir()
    call failed.
    
    Cc: qemu-stable@nongnu.org
    Fixes: b39bce121b (add dirp_map to hide lo_dirp pointers)
    Reported-by: Coverity (CID 1421933 USE_AFTER_FREE)
    Suggested-by: Peter Maydell <peter.maydell@linaro.org>
    Signed-off-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Message-Id: <20200321120654.7985-1-philmd@redhat.com>
    Reviewed-by: Stefan Hajnoczi <stefanha@redhat.com>
    Signed-off-by: Dr. David Alan Gilbert <dgilbert@redhat.com>
    (cherry picked from commit e1cd92d95cd4f97b3464c4e08cd5b22bf5ca05cb)
    Signed-off-by: Danilo C. L. de Paula <ddepaula@redhat.com>

--- tools/virtiofsd/passthrough_ll.c
+++ tools/virtiofsd/passthrough_ll.c
@@ -1524,8 +1524,7 @@ out_err:
     if (d) {
         if (d->dp) {
             closedir(d->dp);
-        }
-        if (fd != -1) {
+        } else if (fd != -1) {
             close(fd);
         }
         free(d);
