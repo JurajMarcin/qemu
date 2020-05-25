559d5899473dea180ced39a32bfbfbf2310c6e04
Marc-André Lureau <marcandre.lureau@redhat.com>
2020-05-25 15:33:06 +0100

    Revert "RHEL: disable hostmem-memfd"
    RH-Author: Marc-André Lureau <marcandre.lureau@redhat.com>
    Message-id: <20200525153306.15373-1-marcandre.lureau@redhat.com>
    Patchwork-id: 96747
    O-Subject: [RHEL-AV-8.2.1 qemu-kvm PATCH] Revert "RHEL: disable hostmem-memfd"
    Bugzilla: 1839030
    RH-Acked-by: Daniel P. Berrange <berrange@redhat.com>
    RH-Acked-by: Sergio Lopez Pascual <slp@redhat.com>
    RH-Acked-by: Stefano Garzarella <sgarzare@redhat.com>
    
    BZ: https://bugzilla.redhat.com/show_bug.cgi?id=1839030
    BRANCH: rhel-av-8.2.1
    UPSTREAM: RHEL-only
    BREW: http://brewweb.devel.redhat.com/brew/taskinfo?taskID=28817132
    
    This reverts commit f7587ddb9a2731bf678a24156b6285dda79a4b2b.
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Signed-off-by: Danilo C. L. de Paula <ddepaula@redhat.com>

--- util/memfd.c
+++ util/memfd.c
@@ -193,7 +193,7 @@ bool qemu_memfd_alloc_check(void)
  */
 bool qemu_memfd_check(unsigned int flags)
 {
-#if 0 /* RHEL: memfd support disabled */
+#ifdef CONFIG_LINUX
     int mfd = memfd_create("test", flags | MFD_CLOEXEC);
 
     if (mfd >= 0) {
