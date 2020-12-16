58688d868656e77f67ea915544b0bb3bb60f33d8
Marc-André Lureau <marcandre.lureau@redhat.com>
2020-12-16 16:06:11 -0500

    qga: add command guest-get-disks
    RH-Author: Marc-André Lureau <marcandre.lureau@redhat.com>
    Message-id: <20201216160615.324213-7-marcandre.lureau@redhat.com>
    Patchwork-id: 100475
    O-Subject: [RHEL-8.4.0 qemu-kvm PATCH v2 06/10] qga: add command guest-get-disks
    Bugzilla: 1859494
    RH-Acked-by: Danilo de Paula <ddepaula@redhat.com>
    RH-Acked-by: Sergio Lopez Pascual <slp@redhat.com>
    RH-Acked-by: Stefan Hajnoczi <stefanha@redhat.com>
    
    From: Tomáš Golembiovský <tgolembi@redhat.com>
    
    Add API and stubs for new guest-get-disks command.
    
    The command guest-get-fsinfo can be used to list information about disks
    and partitions but it is limited only to mounted disks with filesystem.
    This new command should allow listing information about disks of the VM
    regardles whether they are mounted or not. This can be usefull for
    management applications for mapping virtualized devices or pass-through
    devices to device names in the guest OS.
    
    Signed-off-by: Tomáš Golembiovský <tgolembi@redhat.com>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Reviewed-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Signed-off-by: Michael Roth <michael.roth@amd.com>
    
    (cherry-picked from commit c27ea3f9ef7c7f29e55bde91879f8514abce9c38)
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Signed-off-by: Danilo C. L. de Paula <ddepaula@redhat.com>

--- qga/commands-posix.c
+++ qga/commands-posix.c
@@ -3039,3 +3039,9 @@ GuestOSInfo *qmp_guest_get_osinfo(Error **errp)
 
     return info;
 }
+
+GuestDiskInfoList *qmp_guest_get_disks(Error **errp)
+{
+    error_setg(errp, QERR_UNSUPPORTED);
+    return NULL;
+}
