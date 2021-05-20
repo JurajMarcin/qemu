73b1578882a9155016bcac9d5d05b34e12c86c1a
Auger Eric <eric.auger@redhat.com>
2021-05-20 19:23:25 -0400

    hw/arm/virt: Add 8.5 machine type
    RH-Author: Auger Eric <eric.auger@redhat.com>
    Message-id: <20210520192328.381798-2-eric.auger@redhat.com>
    Patchwork-id: 101610
    O-Subject: [RHEL-AV-8.5.0 PATCH v2 1/4] hw/arm/virt: Add 8.5 machine type
    Bugzilla: 1957667
    RH-Acked-by: Miroslav Rezanina <mrezanin@redhat.com>
    RH-Acked-by: Andrew Jones <drjones@redhat.com>
    RH-Acked-by: Gavin Shan <gshan@redhat.com>
    
    branch: rhel-av-8.5.0
    Bugzilla: 1957667
    Brew: 36915661
    Upstream: no
    
    Add 8.5 machine type.
    
    Signed-off-by: Eric Auger <eric.auger@redhat.com>
    Signed-off-by: Danilo C. L. de Paula <ddepaula@redhat.com>

--- hw/arm/virt.c
+++ hw/arm/virt.c
@@ -3094,12 +3094,18 @@ static void rhel_machine_init(void)
 }
 type_init(rhel_machine_init);
 
-static void rhel840_virt_options(MachineClass *mc)
+static void rhel850_virt_options(MachineClass *mc)
 {
     compat_props_add(mc->compat_props, arm_rhel_compat, arm_rhel_compat_len);
+}
+DEFINE_RHEL_MACHINE_AS_LATEST(8, 5, 0)
+
+static void rhel840_virt_options(MachineClass *mc)
+{
+    rhel850_virt_options(mc);
     compat_props_add(mc->compat_props, hw_compat_rhel_8_4, hw_compat_rhel_8_4_len);
 }
-DEFINE_RHEL_MACHINE_AS_LATEST(8, 4, 0)
+DEFINE_RHEL_MACHINE(8, 4, 0)
 
 static void rhel830_virt_options(MachineClass *mc)
 {
