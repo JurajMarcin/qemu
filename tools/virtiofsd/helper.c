6abfb7b3c37015ff901d11f178bc6900deec2acf
Max Reitz <mreitz@redhat.com>
2021-08-06 11:58:27 -0400

    virtiofsd: Fix the help message of posix lock
    RH-Author: Max Reitz <mreitz@redhat.com>
    Message-id: <20210806115827.740945-3-mreitz@redhat.com>
    Patchwork-id: 101969
    O-Subject: [RHEL-8.5.0 qemu-kvm PATCH 2/2] virtiofsd: Fix the help message of posix lock
    Bugzilla: 1967496
    RH-Acked-by: Stefano Garzarella <sgarzare@redhat.com>
    RH-Acked-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    RH-Acked-by: Vivek Goyal <vgoyal@redhat.com>
    
    From: Jiachen Zhang <zhangjiachen.jaycee@bytedance.com>
    
    The commit 88fc107956a5812649e5918e0c092d3f78bb28ad disabled remote
    posix locks by default. But the --help message still says it is enabled
    by default. So fix it to output no_posix_lock.
    
    Signed-off-by: Jiachen Zhang <zhangjiachen.jaycee@bytedance.com>
    Message-Id: <20201027081558.29904-1-zhangjiachen.jaycee@bytedance.com>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Reviewed-by: Dr. David Alan Gilbert <dgilbert@redhat.com>
    Signed-off-by: Dr. David Alan Gilbert <dgilbert@redhat.com>
    (cherry picked from commit 0429eaf518be1d4742356056e6c886b7f9bc9712)
    Signed-off-by: Max Reitz <mreitz@redhat.com>
    Signed-off-by: Danilo C. L. de Paula <ddepaula@redhat.com>

--- tools/virtiofsd/helper.c
+++ tools/virtiofsd/helper.c
@@ -163,7 +163,7 @@ void fuse_cmdline_help(void)
            "                               default: false\n"
            "    -o posix_lock|no_posix_lock\n"
            "                               enable/disable remote posix lock\n"
-           "                               default: posix_lock\n"
+           "                               default: no_posix_lock\n"
            "    -o readdirplus|no_readdirplus\n"
            "                               enable/disable readirplus\n"
            "                               default: readdirplus except with "
