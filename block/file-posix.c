JurajMarcin/qemu

commit 01ef8185b809af9d287e1a03a3f9d8ea8231118a

scsi-generic: pass max_segments via max_iov field in BlockLimits
I/O to a disk via read/write is not limited by the number of segments allowed
by the host adapter; the kernel can split requests if needed, and the limit
imposed by the host adapter can be very low (256k or so) to avoid that SG_IO
returns EINVAL if memory is heavily fragmented.

Since this value is only interesting for SG_IO-based I/O, do not include
it in the max_transfer and only take it into account when patching the
block limits VPD page in the scsi-generic device.

Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
Reviewed-by: Max Reitz <mreitz@redhat.com>

block/file-posix.c
@@ -1237,8 +1237,7 @@ static void raw_refresh_limits(BlockDriverState *bs, Error **errp)
 
         ret = sg_get_max_segments(s->fd);
         if (ret > 0) {
-            bs->bl.max_transfer = MIN(bs->bl.max_transfer,
-                                      ret * qemu_real_host_page_size);
+            bs->bl.max_iov = ret;
         }
     }
 

hw/scsi/scsi-generic.c
@@ -179,10 +179,12 @@ static int scsi_handle_inquiry_reply(SCSIGenericReq *r, SCSIDevice *s, int len)
         (r->req.cmd.buf[1] & 0x01)) {
         page = r->req.cmd.buf[2];
         if (page == 0xb0) {
-            uint32_t max_transfer =
-                blk_get_max_transfer(s->conf.blk) / s->blocksize;
+            uint32_t max_transfer = blk_get_max_transfer(s->conf.blk);
+            uint32_t max_iov = blk_get_max_iov(s->conf.blk);
 
             assert(max_transfer);
+            max_transfer = MIN_NON_ZERO(max_transfer, max_iov * qemu_real_host_page_size)
+                / s->blocksize;
             stl_be_p(&r->buf[8], max_transfer);
             /* Also take care of the opt xfer len. */
             stl_be_p(&r->buf[12],

commit 1759b1611054e2c6d927f91394c87ca3a7f2d38e

file-posix: Cap max_iov at IOV_MAX
RH-Author: Kevin Wolf <kwolf@redhat.com>
Message-id: <20210823110936.10078-2-kwolf@redhat.com>
Patchwork-id: 102016
O-Subject: [RHEL-AV-8.4.0.z qemu-kvm PATCH 1/1] file-posix: Cap max_iov at IOV_MAX
Bugzilla: 1996131
RH-Acked-by: Philippe Mathieu-Daudé <philmd@redhat.com>
RH-Acked-by: Stefan Hajnoczi <stefanha@redhat.com>
RH-Acked-by: Hanna Reitz <hreitz@redhat.com>

BlockLimits.max_iov is used both for regular I/O and for SG_IO requests,
which however have different limits: The kernel supports up to IOV_MAX
for regular I/O, and SG_IO depends on the hardware. As long as we're
using the same limit for both, we need to use the smaller one so that
it's safe for both paths.

In upstream, this will be fixed differently during the 6.2 release cycle
by splitting into two separate limits.

Fixes: 01ef8185b809af9d287e1a03a3f9d8ea8231118a
Signed-off-by: Kevin Wolf <kwolf@redhat.com>
Signed-off-by: Danilo C. L. de Paula <ddepaula@redhat.com>

block/file-posix.c
@@ -1263,7 +1263,7 @@ static void raw_refresh_limits(BlockDriverState *bs, Error **errp)
 
         ret = hdev_get_max_segments(s->fd, &st);
         if (ret > 0) {
-            bs->bl.max_iov = ret;
+            bs->bl.max_iov = MIN(ret, IOV_MAX);
         }
     }
 }