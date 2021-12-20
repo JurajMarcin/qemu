47f8fe1b822a658a75be9f3dd618003cd8326512
Eric Auger <eric.auger@redhat.com>
2021-12-20 10:11:47 +0100

    hw/arm/virt: Add 9.0 machine type and remove 8.5 one
    RH-Author: Eric Auger <eric.auger@redhat.com>
    RH-MergeRequest: 57: hw/arm/virt: Add 9.0 machine type and remove 8.5 one
    RH-Commit: [5/6] f573a2fb44882a010e2c6bf5f561f29d54e6e9b5 (eauger1/centos-qemu-kvm)
    RH-Bugzilla: 2031044
    RH-Acked-by: Gavin Shan <gshan@redhat.com>
    RH-Acked-by: Andrew Jones <drjones@redhat.com>
    RH-Acked-by: Miroslav Rezanina <mrezanin@redhat.com>
    
    branch: c9s
    Brew: 42213566
    Upstream: no
    
    Add 9.0 machine type and remove 8.5 one.
    
    Signed-off-by: Eric Auger <eric.auger@redhat.com>
    
    patch_name: kvm-hw-arm-virt-Add-9.0-machine-type-and-remove-8.5-one.patch
    present_in_specfile: true
    location_in_specfile: 32

--- hw/arm/virt.c
+++ hw/arm/virt.c
@@ -3236,14 +3236,8 @@ static void rhel_machine_init(void)
 }
 type_init(rhel_machine_init);
 
-static void rhel850_virt_options(MachineClass *mc)
+static void rhel900_virt_options(MachineClass *mc)
 {
-    VirtMachineClass *vmc = VIRT_MACHINE_CLASS(OBJECT_CLASS(mc));
-
     compat_props_add(mc->compat_props, arm_rhel_compat, arm_rhel_compat_len);
-    compat_props_add(mc->compat_props, hw_compat_rhel_8_5, hw_compat_rhel_8_5_len);
-    mc->smp_props.prefer_sockets = true;
-    vmc->no_cpu_topology = true;
-    vmc->no_tcg_its = true;
 }
-DEFINE_RHEL_MACHINE_AS_LATEST(8, 5, 0)
+DEFINE_RHEL_MACHINE_AS_LATEST(9, 0, 0)
