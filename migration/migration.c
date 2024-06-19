c9eb5f8e86d031060c72aeb9d995844c6f842c58
Peter Xu <peterx@redhat.com>
2024-06-19 18:30:40 -0400

    migration/postcopy: Add postcopy-recover-setup phase
    RH-Author: Juraj Marcin <None>
    RH-MergeRequest: 419: migration: New postcopy state, and some cleanups [rhel-9.5.z]
    RH-Jira: RHEL-63874
    RH-Acked-by: Peter Xu <peterx@redhat.com>
    RH-Acked-by: Miroslav Rezanina <mrezanin@redhat.com>
    RH-Commit: [5/11] ce81d3b247b9f9541a75265a07082394ce419f3a
    
    This patch adds a migration state on src called "postcopy-recover-setup".
    The new state will describe the intermediate step starting from when the
    src QEMU received a postcopy recovery request, until the migration channels
    are properly established, but before the recovery process take place.
    
    The request came from Libvirt where Libvirt currently rely on the migration
    state events to detect migration state changes.  That works for most of the
    migration process but except postcopy recovery failures at the beginning.
    
    Currently postcopy recovery only has two major states:
    
      - postcopy-paused: this is the state that both sides of QEMU will be in
        for a long time as long as the migration channel was interrupted.
    
      - postcopy-recover: this is the state where both sides of QEMU handshake
        with each other, preparing for a continuation of postcopy which used to
        be interrupted.
    
    The issue here is when the recovery port is invalid, the src QEMU will take
    the URI/channels, noticing the ports are not valid, and it'll silently keep
    in the postcopy-paused state, with no event sent to Libvirt.  In this case,
    the only thing Libvirt can do is to poll the migration status with a proper
    interval, however that's less optimal.
    
    Considering that this is the only case where Libvirt won't get a
    notification from QEMU on such events, let's add postcopy-recover-setup
    state to mimic what we have with the "setup" state of a newly initialized
    migration, describing the phase of connection establishment.
    
    With that, postcopy recovery will have two paths to go now, and either path
    will guarantee an event generated.  Now the events will look like this
    during a recovery process on src QEMU:
    
      - Initially when the recovery is initiated on src, QEMU will go from
        "postcopy-paused" -> "postcopy-recover-setup".  Old QEMUs don't have
        this event.
    
      - Depending on whether the channel re-establishment is succeeded:
    
        - In succeeded case, src QEMU will move from "postcopy-recover-setup"
          to "postcopy-recover".  Old QEMUs also have this event.
    
        - In failure case, src QEMU will move from "postcopy-recover-setup" to
          "postcopy-paused" again.  Old QEMUs don't have this event.
    
    This guarantees that Libvirt will always receive a notification for
    recovery process properly.
    
    One thing to mention is, such new status is only needed on src QEMU not
    both.  On dest QEMU, the state machine doesn't change.  Hence the events
    don't change either.  It's done like so because dest QEMU may not have an
    explicit point of setup start.  E.g., it can happen that when dest QEMUs
    doesn't use migrate-recover command to use a new URI/channel, but the old
    URI/channels can be reused in recovery, in which case the old ports simply
    can work again after the network routes are fixed up.
    
    Add a new helper postcopy_is_paused() detecting whether postcopy is still
    paused, taking RECOVER_SETUP into account too.  When using it on both
    src/dst, a slight change is done altogether to always wait for the
    semaphore before checking the status, because for both sides a sem_post()
    will be required for a recovery.
    
    Cc: Jiri Denemark <jdenemar@redhat.com>
    Cc: Prasad Pandit <ppandit@redhat.com>
    Reviewed-by: Fabiano Rosas <farosas@suse.de>
    Buglink: https://issues.redhat.com/browse/RHEL-38485
    Signed-off-by: Peter Xu <peterx@redhat.com>
    Signed-off-by: Fabiano Rosas <farosas@suse.de>
    
    (cherry picked from commit 4146b77ec7640d3c30d42558e13423594b114385)
    
    JIRA: https://issues.redhat.com/browse/RHEL-63874
    Y-JIRA: https://issues.redhat.com/browse/RHEL-38485
    
    Signed-off-by: Juraj Marcin <jmarcin@redhat.com>

--- migration/migration.c
+++ migration/migration.c
@@ -1100,6 +1100,7 @@ bool migration_is_setup_or_active(void)
     case MIGRATION_STATUS_ACTIVE:
     case MIGRATION_STATUS_POSTCOPY_ACTIVE:
     case MIGRATION_STATUS_POSTCOPY_PAUSED:
+    case MIGRATION_STATUS_POSTCOPY_RECOVER_SETUP:
     case MIGRATION_STATUS_POSTCOPY_RECOVER:
     case MIGRATION_STATUS_SETUP:
     case MIGRATION_STATUS_PRE_SWITCHOVER:
@@ -1122,6 +1123,7 @@ bool migration_is_running(void)
     case MIGRATION_STATUS_ACTIVE:
     case MIGRATION_STATUS_POSTCOPY_ACTIVE:
     case MIGRATION_STATUS_POSTCOPY_PAUSED:
+    case MIGRATION_STATUS_POSTCOPY_RECOVER_SETUP:
     case MIGRATION_STATUS_POSTCOPY_RECOVER:
     case MIGRATION_STATUS_SETUP:
     case MIGRATION_STATUS_PRE_SWITCHOVER:
@@ -1273,6 +1275,7 @@ static void fill_source_migration_info(MigrationInfo *info)
     case MIGRATION_STATUS_PRE_SWITCHOVER:
     case MIGRATION_STATUS_DEVICE:
     case MIGRATION_STATUS_POSTCOPY_PAUSED:
+    case MIGRATION_STATUS_POSTCOPY_RECOVER_SETUP:
     case MIGRATION_STATUS_POSTCOPY_RECOVER:
         /* TODO add some postcopy stats */
         populate_time_info(info, s);
@@ -1469,10 +1472,31 @@ static void migrate_error_free(MigrationState *s)
 
 static void migrate_fd_error(MigrationState *s, const Error *error)
 {
+    MigrationStatus current = s->state;
+    MigrationStatus next;
+
     trace_migrate_fd_error(error_get_pretty(error));
     assert(s->to_dst_file == NULL);
-    migrate_set_state(&s->state, MIGRATION_STATUS_SETUP,
-                      MIGRATION_STATUS_FAILED);
+
+    switch (current) {
+    case MIGRATION_STATUS_SETUP:
+        next = MIGRATION_STATUS_FAILED;
+        break;
+    case MIGRATION_STATUS_POSTCOPY_RECOVER_SETUP:
+        /* Never fail a postcopy migration; switch back to PAUSED instead */
+        next = MIGRATION_STATUS_POSTCOPY_PAUSED;
+        break;
+    default:
+        /*
+         * This really shouldn't happen. Just be careful to not crash a VM
+         * just for this.  Instead, dump something.
+         */
+        error_report("%s: Illegal migration status (%s) detected",
+                     __func__, MigrationStatus_str(current));
+        return;
+    }
+
+    migrate_set_state(&s->state, current, next);
     migrate_set_error(s, error);
 }
 
@@ -1573,6 +1597,7 @@ bool migration_in_postcopy(void)
     switch (s->state) {
     case MIGRATION_STATUS_POSTCOPY_ACTIVE:
     case MIGRATION_STATUS_POSTCOPY_PAUSED:
+    case MIGRATION_STATUS_POSTCOPY_RECOVER_SETUP:
     case MIGRATION_STATUS_POSTCOPY_RECOVER:
         return true;
     default:
@@ -1964,6 +1989,9 @@ static bool migrate_prepare(MigrationState *s, bool blk, bool blk_inc,
                        "when release-ram capability is set");
             return false;
         }
+
+        migrate_set_state(&s->state, MIGRATION_STATUS_POSTCOPY_PAUSED,
+                          MIGRATION_STATUS_POSTCOPY_RECOVER_SETUP);
 
         /* This is a resume, skip init status */
         return true;
@@ -3020,9 +3048,9 @@ static MigThrError postcopy_pause(MigrationState *s)
          * We wait until things fixed up. Then someone will setup the
          * status back for us.
          */
-        while (s->state == MIGRATION_STATUS_POSTCOPY_PAUSED) {
+        do {
             qemu_sem_wait(&s->postcopy_pause_sem);
-        }
+        } while (postcopy_is_paused(s->state));
 
         if (s->state == MIGRATION_STATUS_POSTCOPY_RECOVER) {
             /* Woken up by a recover procedure. Give it a shot */
@@ -3687,7 +3715,7 @@ void migrate_fd_connect(MigrationState *s, Error *error_in)
 {
     Error *local_err = NULL;
     uint64_t rate_limit;
-    bool resume = s->state == MIGRATION_STATUS_POSTCOPY_PAUSED;
+    bool resume = (s->state == MIGRATION_STATUS_POSTCOPY_RECOVER_SETUP);
     int ret;
 
     /*
@@ -3754,7 +3782,7 @@ void migrate_fd_connect(MigrationState *s, Error *error_in)
 
     if (resume) {
         /* Wakeup the main migration thread to do the recovery */
-        migrate_set_state(&s->state, MIGRATION_STATUS_POSTCOPY_PAUSED,
+        migrate_set_state(&s->state, MIGRATION_STATUS_POSTCOPY_RECOVER_SETUP,
                           MIGRATION_STATUS_POSTCOPY_RECOVER);
         qemu_sem_post(&s->postcopy_pause_sem);
         return;
