01ffa96c3b6c48a1160420bf32d0422f63567a2d
Thomas Huth <thuth@redhat.com>
2024-06-24 14:15:08 +0200

    target/s390x/cpu_models: Disable everything up to the z12 CPU model
    RH-Author: Thomas Huth <thuth@redhat.com>
    RH-MergeRequest: 252: s390x: remove legacy CPU types
    RH-Jira: RHEL-39898
    RH-Acked-by: Cédric Le Goater <clg@redhat.com>
    RH-Acked-by: Miroslav Rezanina <mrezanin@redhat.com>
    RH-Commit: [4/5] f5236c8041bfcb63df4046f7bb0a12c1fa90062d (thuth/qemu-kvm-cs9)
    
    Upstream-Status: N/A
    JIRA: https://issues.redhat.com/browse/RHEL-39898
    
    When RHEL 10.0 gets released, the z14 will be the oldest mainframe
    that is still officially supported by IBM, see:
    https://www.ibm.com/support/pages/ibm-mainframe-life-cycle-history
    
    Now each IBM Z machine can "emulate" the previous two CPU types in
    hardware for virtual guests, so we should still allow the z12 and
    z13 in our qemu-kvm builds, too. But everything that is older than
    the z12 can be disabled now.
    
    Signed-off-by: Thomas Huth <thuth@redhat.com>
    
    Patch-name: kvm-target-s390x-cpu_models-Disable-everything-up-to-the.patch
    Patch-id: 29
    Patch-present-in-specfile: True

--- target/s390x/cpu_models.c
+++ target/s390x/cpu_models.c
@@ -75,7 +75,6 @@ static S390CPUDef s390_cpu_defs[] = {
     CPUDEF_INIT(0x2096, 9, 2, 40, 0x00000000U, "z9BC", "IBM System z9 BC GA1"),
     CPUDEF_INIT(0x2094, 9, 3, 40, 0x00000000U, "z9EC.3", "IBM System z9 EC GA3"),
     CPUDEF_INIT(0x2096, 9, 3, 40, 0x00000000U, "z9BC.2", "IBM System z9 BC GA2"),
-#endif
     CPUDEF_INIT(0x2097, 10, 1, 43, 0x00000000U, "z10EC", "IBM System z10 EC GA1"),
     CPUDEF_INIT(0x2097, 10, 2, 43, 0x00000000U, "z10EC.2", "IBM System z10 EC GA2"),
     CPUDEF_INIT(0x2098, 10, 2, 43, 0x00000000U, "z10BC", "IBM System z10 BC GA1"),
@@ -84,6 +83,7 @@ static S390CPUDef s390_cpu_defs[] = {
     CPUDEF_INIT(0x2817, 11, 1, 44, 0x08000000U, "z196", "IBM zEnterprise 196 GA1"),
     CPUDEF_INIT(0x2817, 11, 2, 44, 0x08000000U, "z196.2", "IBM zEnterprise 196 GA2"),
     CPUDEF_INIT(0x2818, 11, 2, 44, 0x08000000U, "z114", "IBM zEnterprise 114 GA1"),
+#endif
     CPUDEF_INIT(0x2827, 12, 1, 44, 0x08000000U, "zEC12", "IBM zEnterprise EC12 GA1"),
     CPUDEF_INIT(0x2827, 12, 2, 44, 0x08000000U, "zEC12.2", "IBM zEnterprise EC12 GA2"),
     CPUDEF_INIT(0x2828, 12, 2, 44, 0x08000000U, "zBC12", "IBM zEnterprise BC12 GA1"),
