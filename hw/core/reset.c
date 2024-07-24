4a795885dba58965ec7f7d1148ee78b5f14250b5
Juraj Marcin <jmarcin@redhat.com>
2024-07-24 16:30:22 +0200

    reset: Use ResetType for qemu_devices_reset() and MachineClass->reset()
    Currently, both qemu_devices_reset() and MachineClass->reset() use
    ShutdownCause for the reason of the reset. However, the Resettable
    interface uses ResetState, so ShutdownCause needs to be translated to
    ResetType somewhere. Translating it qemu_devices_reset() makes adding
    new reset types harder, as they cannot always be matched to a single
    ShutdownCause here, and devices may need to check the ResetType to
    determine what to reset and if to reset at all.
    
    This patch moves this translation up in the call stack to
    qemu_system_reset() and updates all MachineClass children to use the
    ResetType instead.
    
    Signed-off-by: Juraj Marcin <jmarcin@redhat.com>

--- hw/core/reset.c
+++ hw/core/reset.c
@@ -25,8 +25,8 @@
 
 #include "qemu/osdep.h"
 #include "sysemu/reset.h"
-#include "hw/resettable.h"
 #include "hw/core/resetcontainer.h"
+#include "hw/resettable.h"
 
 /*
  * Return a pointer to the singleton container that holds all the Resettable
@@ -170,11 +170,8 @@ void qemu_unregister_resettable(Object *obj)
     resettable_container_remove(get_root_reset_container(), obj);
 }
 
-void qemu_devices_reset(ShutdownCause reason)
+void qemu_devices_reset(ResetType type)
 {
-    ResetType type = (reason == SHUTDOWN_CAUSE_SNAPSHOT_LOAD) ?
-        RESET_TYPE_SNAPSHOT_LOAD : RESET_TYPE_COLD;
-
     /* Reset the simulation */
     resettable_reset(OBJECT(get_root_reset_container()), type);
 }
