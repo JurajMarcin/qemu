947acf7b1e5747f1efde069feeceee377fa13bc8
Michael S. Tsirkin <mst@redhat.com>
2024-11-06 17:29:35 -0500

    virtio-net: disable USO for all RHEL9
    RH-Author: MST <mst@redhat.com>
    RH-MergeRequest: 319: virtio-net migration: ensure compatibility of rhel9*
    RH-Jira: RHEL-69500
    RH-Acked-by: Peter Xu <peterx@redhat.com>
    RH-Acked-by: Miroslav Rezanina <mrezanin@redhat.com>
    RH-Commit: [1/1] c7518d8293e1cfdd928f0ce454249ecb7bd7117a (mstredhat/qemu-kvm-centos)
    
    With commit 298dae99b77f ("virtio-net: disable USO for RHEL9")
    we fixed migration for 9.6 but we forgot to apply the fix
    for 9.6 when it was created.
    
    To help us not forget in 9.7 and beyond, create a generic RHEL9 compat
    and apply it.
    
    Upstream status: n/a: upstream has no guarantee if kernel features change
    Tested: lightly on developer's machine.
    JIRA: https://issues.redhat.com/browse/RHEL-69500
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    
    Patch-name: kvm-virtio-net-disable-USO-for-RHEL9.patch
    Patch-id: 83
    Patch-present-in-specfile: True
    
    Patch-name: kvm-virtio-net-disable-USO-for-all-RHEL9.patch
    Patch-id: 118
    Patch-present-in-specfile: True

--- hw/arm/virt.c
+++ hw/arm/virt.c
@@ -3595,6 +3595,9 @@ DEFINE_VIRT_MACHINE_AS_LATEST(10, 0, 0)
 static void virt_rhel_machine_9_6_0_options(MachineClass *mc)
 {
     virt_rhel_machine_10_0_0_options(mc);
+
+    /* NB: remember to move this line to the *latest* RHEL 9 machine */
+    compat_props_add(mc->compat_props, hw_compat_rhel_9, hw_compat_rhel_9_len);
 }
 DEFINE_VIRT_MACHINE(9, 6, 0)
 
